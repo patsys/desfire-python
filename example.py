a1387c3807b45b4d192be4ac0dd979b7cf4b69ad
pweber <pat.weber91@gmail.com>
2018-05-01 21:19:23 +0200

    add logger information to example

--- example.py
+++ example.py
@@ -28,6 +28,7 @@ from smartcard.util import toHexString
 from smartcard.CardConnectionObserver import ConsoleCardConnectionObserver
 
 from Desfire.DESFire import *
+from Desfire.util import byte_array_to_human_readable_hex
 from Desfire.pcsc import PCSCDevice
 
 IGNORE_EXCEPTIONS = (KeyboardInterrupt, MemoryError,)
@@ -81,34 +82,54 @@ class MyObserver(CardObserver):
             logger.info("Opened connection %s", connection.component)
             desfire = DESFire(PCSCDevice(connection.component))
             key_setting=desfire.getKeySetting()
-            desfire.authenticate(0,key_setting,'84 9B 36 C5 F8 BF 4A 09')
-            desfire.getCardVersion()
+            logger.info('Auth Key %d',0)
+            desfire.authenticate(0,key_setting)
+            info=desfire.getCardVersion()
+            logger.info(info)
+            logger.info('Format card')
             desfire.formatCard()
+            logger.info('Create application with ID 00AE16')
             desfire.createApplication("00 AE 16",[DESFireKeySettings.KS_ALLOW_CHANGE_MK,DESFireKeySettings.KS_LISTING_WITHOUT_MK,DESFireKeySettings.KS_CONFIGURATION_CHANGEABLE],14,DESFireKeyType.DF_KEY_AES)
+            logger.info('Select application with ID 00AE16')
             desfire.selectApplication('00 AE 16')
             default_key=desfire.createKeySetting('00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00',0,DESFireKeyType.DF_KEY_AES,[])            
             app_key=desfire.createKeySetting('00 10 20 31 40 50 60 70 80 90 A0 B0 B0 A0 90 80',0,DESFireKeyType.DF_KEY_AES,[])
+            logger.info('Auth Key %d',0)
             desfire.authenticate(0,default_key)
+            logger.info('Cange Key %d',0)
             desfire.changeKey(0,app_key,default_key)
+            logger.info('Auth Key %d',0)
             desfire.authenticate(0,app_key)
             desfire.changeKeySettings([ DESFireKeySettings.KS_ALLOW_CHANGE_MK, DESFireKeySettings.KS_CONFIGURATION_CHANGEABLE, DESFireKeySettings.KS_CHANGE_KEY_WITH_KEY_1])
             app_key_1=desfire.createKeySetting('11 22 33 44 55 66 77 88 99 AA BB CC DD EE FF 00',0,DESFireKeyType.DF_KEY_AES,[])
+            logger.info('Cange Key %d',1)
             desfire.changeKey(1,app_key_1,default_key)
+            logger.info('Auth Key %d',1)
             desfire.authenticate(1,app_key_1)
             app_key_2=desfire.createKeySetting('22 33 44 55 66 77 88 99 AA BB CC DD EE FF 00 11',0,DESFireKeyType.DF_KEY_AES,[])
+            logger.info('Cange Key %d',2)
             desfire.changeKey(2,app_key_2,default_key)
             app_key_3=desfire.createKeySetting('33 44 55 66 77 88 99 AA BB CC DD EE FF 00 11 22',0,DESFireKeyType.DF_KEY_AES,[])
+            logger.info('Cange Key %d',3)
             desfire.changeKey(3,app_key_3,default_key)
             app_key_4=desfire.createKeySetting('44 55 66 77 88 99 AA BB CC DD EE FF 00 11 22 33',0,DESFireKeyType.DF_KEY_AES,[])
+            logger.info('Cange Key %d',4)
             desfire.changeKey(4,app_key_4,default_key)
+            logger.info('Auth Key %d',0)
             desfire.authenticate(0,app_key)
             filePerm=DESFireFilePermissions()
             filePerm.setPerm(0x04,0x03,0x0F,0x02) # key 4 read, key3 write, no key read and write, key2 change permissions
+            logger.info('Creat File with ID %d and %d byte',0,32)
             desfire.createStdDataFile(0,filePerm,32) # file Id 0, length 32 byte
+            logger.info('Auth Key %d',3)
             desfire.authenticate(3,app_key_3)
-            desfire.writeFileData(0,0,32,'00 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F 10 11 12 13 14 15 16 17 18 19 1A 1B 1C 1D 1E 1F 20')
+            write='00 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F 10 11 12 13 14 15 16 17 18 19 1A 1B 1C 1D 1E 1F 20'
+            logger.info('Data write %s',write)
+            desfire.writeFileData(0,0,32,write)
+            logger.info('Auth Key %d',4)
             desfire.authenticate(4,app_key_4)
-            desfire.readFileData(0,0,32)
+            read=desfire.readFileData(0,0,32)
+            logger.info('Data read %s',byte_array_to_human_readable_hex(read))
 
 
 
@@ -119,7 +140,7 @@ class MyObserver(CardObserver):
 def main():
     global logger
 
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
 
     logger.info("Insert MIFARE Desfire card to any reader to get its applications.")
