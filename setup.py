d9a7f812813e8caf56c5414a9a49335136ec009c
patsys <pat.weber91@googlemail.com>
2018-05-13 22:31:17 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='DESFfire',
+	version='0.11',
+	packages=find_packages(exclude=['tests*','examples*']),
+	license='MIT',
+	description='DESFire library for python',
+	long_description=open('README.txt').read(),
+	install_requires=['pycrypto','enum34','pyscard'],
+	url='https://github.com/patsys/desfire-python',
+	author='Patrick Weber',
+	author_email='pat.weber91@gmail.com'
+)
